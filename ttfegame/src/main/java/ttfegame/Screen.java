bc830ada2e81158990b28b9175fb9f05f1600117
ezyxip <anzej2003@gmail.com>
2022-05-25 22:33:10 +0600

    Работа продолжается

--- ttfegame/src/main/java/ttfegame/Screen.java
+++ ttfegame/src/main/java/ttfegame/Screen.java
@@ -1,7 +1,15 @@
 
 package ttfegame;
 
+import javax.swing.JFrame;
+
 
 public class Screen {
+    private JFrame window;
+    void setActive() {
+        window = GameWindow.getMainWindow();
+        //Рисование на окне
+        
+    }
     
 }
